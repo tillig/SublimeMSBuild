f5abf2009394ecc145bf9c435a83e0635df625e0
Travis Illig <tillig@paraesthesia.com>
2012-07-19 09:35:09 -0700

    Ignoring packages.json for the sublime-package.
    Excluding packages.json so people who install the package without the
    package manager won't have things auto-upgraded from under them.

--- create-package.ps1
+++ create-package.ps1
@@ -1,4 +1,4 @@
 # This script assumes use of the PowerShell Community Extensions Module
 # http://pscx.codeplex.com/
 Import-Module Pscx
-Get-ChildItem -Exclude .git*,*.ps1 | Write-Zip -Level 9 -OutputPath MSBuild.sublime-package
+Get-ChildItem -Exclude .git*,*.ps1,packages.json | Write-Zip -Level 9 -OutputPath MSBuild.sublime-package
